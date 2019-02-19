1c1e6f6bf34b807dd68d3732750091bef1d649f9
vladyn <vlado@vlado1.com>
2019-02-19 22:33:23 +0200

    eslint linter extending the airbnb one

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "eslint-config-airbnb",
+    "globals": {
+        "fetch": false
+    },
+    "env": {
+        "browser": true
+    },
+    "rules": {
+        "no-restricted-syntax": 1,
+        "no-console": 0,
+        "no-unused-vars": 1,
+        "max-len": 1,
+        "quotes": 0,
+        "no-restricted-syntax": 0,
+    }
+};
