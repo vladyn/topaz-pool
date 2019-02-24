8a89b0b749ec07230a7aa888109789cbc995220c
vladyn <vlado@vlado1.com>
2019-02-24 12:14:24 +0200

    Clearing unused constants

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,10 @@
-export const API_URL = 'https://api.flickr.com/services/rest/';
-export const KEY = '0d2ae097096ffa5edce5e1178ac6295c';
-export const SECRET = '86a4c15b481d6125';
+export const API_URL = 'https://api.flickr.com/services/rest/?';
 export const DETAILS = {
   method: 'flickr.groups.pools.getPhotos',
+  api_key: '0d2ae097096ffa5edce5e1178ac6295c',
   group_id: '929299@N25',
-  user_id: '929299@N25',
-  format: 'json',
-  callback: '1',
   page: '1',
   per_page: '42',
+  format: 'json',
+  nojsoncallback: '1',
 };
