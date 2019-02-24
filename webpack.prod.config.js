a6a3067b2f751a1716c90dc783385e9b36b116fe
vladyn <vlado@vlado1.com>
2019-02-24 20:43:49 +0200

    Adding production mode

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,3 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackExcludeAssetsPlugin = require('html-webpack-exclude-assets-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  // TODO: Implement config for production ready code
+  mode: 'production',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader',
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        { loader: "style-loader" },
+        { loader: "css-loader" },
+        { loader: "sass-loader" },
+      ],
+    },
+    {
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, "css-loader"],
+    },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      excludeAssets: [/doc.js/, /doc.html/],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new HtmlWebpackExcludeAssetsPlugin(),
+  ],
 };
